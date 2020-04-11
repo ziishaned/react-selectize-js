e9b717186dc4e44bfac4670e462980949fa8f50d
Zeeshan <ziishaned@gmail.com>
2020-04-11 16:22:44 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Demo
+
+```js
+import { Select } from 'react-selectize-js';
+
+<Select onChange={(event) => console.log(event.target.value)}>
+  <option value="1">Chuck Tesla</option>
+  <option value="2">Elon Musk</option>
+  <option value="3">Paweł Kuna</option>
+  <option value="4">Nikola Tesla</option>
+</Select>
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+import "selectize";
+import $ from 'jquery';
+import 'selectize/dist/css/selectize.css';
+import React, {ReactElement, useEffect, useRef} from 'react';
+
+export function Select(props): ReactElement {
+  const selectInput = useRef(null);
+
+  useEffect(() => {
+    const {current: selectInputRef} = selectInput;
+    const $select = $(selectInputRef);
+
+    $select.selectize();
+    $select.on('change', props.onChange);
+
+    return () => {
+      selectInputRef.selectize.destroy()
+    };
+  }, [selectInput, props.onChange]);
+
+  return (
+    <select ref={selectInput} value={props.value} className={props.className}>
+      {props.children}
+    </select>
+  );
+}
